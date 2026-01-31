69a739c238a98014ce3bd2739954e61cd26f42e8
DeerMaximum <git983456@parabelmail.de>
2026-01-31 22:57:57 +0100

    Move coordinator to separate file

--- custom_components/ta_coe/__init__.py
+++ custom_components/ta_coe/__init__.py
@@ -2,14 +2,12 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_HOST, STATE_OFF, STATE_ON, Platform
+from homeassistant.const import CONF_HOST, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
-from ta_cmi import ApiError, ChannelMode, CoE, CoEChannel
+from ta_cmi import CoE
 
 from .const import (
     _LOGGER,
@@ -21,6 +19,7 @@ from .const import (
     TYPE_BINARY,
     TYPE_SENSOR,
 )
+from .coordinator import CoEDataUpdateCoordinator
 from .issues import check_coe_server_2x_issue
 from .refresh_task import RefreshTask
 from .state_observer import StateObserver
@@ -104,78 +103,3 @@ async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> Non
     await hass.config_entries.async_reload(entry.entry_id)
 
 
-class CoEDataUpdateCoordinator(DataUpdateCoordinator):
-    """Class to manage fetching CoE data."""
-
-    channel_count: dict[int, int] = {}
-
-    def __init__(
-        self,
-        hass: HomeAssistant,
-        config_entry: ConfigEntry,
-        coe: CoE,
-        can_ids: list[int],
-        update_interval: timedelta,
-    ) -> None:
-        """Initialize."""
-        self.config_entry = config_entry
-
-        self.coe = coe
-        self.can_ids = can_ids
-
-        _LOGGER.debug("Used update interval: %s", update_interval)
-
-        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
-
-    @staticmethod
-    def _format_input(target_channel: CoEChannel) -> tuple[str, str]:
-        """Format the unit and value."""
-        unit: str = target_channel.get_unit()
-        value: str | float = target_channel.value
-
-        if unit == "On/Off":
-            unit = ""
-            if bool(value):
-                value = STATE_ON
-            else:
-                value = STATE_OFF
-
-        if unit == "No/Yes":
-            unit = ""
-            if bool(value):
-                value = "yes"
-            else:
-                value = "no"
-
-        return value, unit
-
-    @staticmethod
-    def _get_type(mode: ChannelMode) -> str:
-        """Get the data type."""
-        if mode is ChannelMode.ANALOG:
-            return TYPE_SENSOR
-
-        return TYPE_BINARY
-
-    async def _async_update_data(self) -> dict[int, Any]:
-        """Update data."""
-        try:
-            return_data: dict[int, dict[str, Any]] = {}
-            _LOGGER.debug("Try to update CoE")
-
-            for can_id in self.can_ids:
-                return_data[can_id] = {TYPE_BINARY: {}, TYPE_SENSOR: {}}
-
-                await self.coe.update(can_id)
-
-                for mode in ChannelMode:
-                    for index, channel in self.coe.get_channels(can_id, mode).items():
-                        value, unit = self._format_input(channel)
-                        return_data[can_id][self._get_type(mode)][index] = {
-                            "value": value,
-                            "unit": unit,
-                        }
-
-            return return_data
-        except ApiError as err:
-            raise UpdateFailed(err) from err
