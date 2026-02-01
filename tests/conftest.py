6d2cde9d0305c0ba5305d6a99c3883cc98b3ee42
DeerMaximum <git983456@parabelmail.de>
2026-02-01 13:26:29 +0100

    Add fixture for mock_setup_entry

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 """Fixtures for testing."""
 
 from copy import deepcopy
+from typing import Generator
 from unittest import mock
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from homeassistant.core import HomeAssistant
@@ -28,6 +30,13 @@ def patch_coe_server_check(request):
 
     request.addfinalizer(unpatch)
 
+@pytest.fixture
+def mock_setup_entry() -> Generator[AsyncMock]:
+    """Override async_setup_entry."""
+    with patch(
+        "custom_components.ta_coe.async_setup_entry", return_value=True
+    ) as mock_setup_entry:
+        yield mock_setup_entry
 
 @pytest.fixture
 def mock_config_entry(hass: HomeAssistant) -> MockConfigEntry:
