ed77b37396aaad65265e72ba2256cf3555d5da5e
DeerMaximum <git983456@parabelmail.de>
2026-02-06 11:08:34 +0100

    Remove default content

--- web-ui/src/app/app.ts
+++ web-ui/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('web-ui');
+
 }
