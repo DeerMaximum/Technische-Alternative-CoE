2d9d3c53f2699d0859d0b9cd6694e9c76ec7020b
DeerMaximum <git983456@parabelmail.de>
2026-02-06 15:07:46 +0100

    Change app-dropdown to select2

--- web-ui/src/app/app.ts
+++ web-ui/src/app/app.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Dropdown} from './components/dropdown/dropdown';
+import {Dropdown, DropdownValues} from './components/dropdown/dropdown';
 import {EntityConfigList} from './components/entity-config-list/entity-config-list';
 
 
@@ -13,8 +13,8 @@ import {EntityConfigList} from './components/entity-config-list/entity-config-li
   styleUrl: './app.scss'
 })
 export class App {
-  sample_config_entries = new Map<string, string>([
-    ["entry_id_one", "First Entry"], ["entry_id_two", "Second Entry"]]
-  );
-
+  sample_config_entries: DropdownValues = [
+    {value: "entry_id_one", label: "First Entry"},
+    {value: "entry_id_two", label: "Second Entry"},
+  ];
 }
--- web-ui/src/app/components/dropdown/dropdown.ts
+++ web-ui/src/app/components/dropdown/dropdown.ts
@@ -1,25 +1,28 @@
 import {Component, input, model} from '@angular/core';
+import {Select2, Select2UpdateEvent} from 'ng-select2-component';
 
+
+export interface DropdownValue {
+  value: string;
+  label: string;
+}
+export type DropdownValues = DropdownValue[]
 
 @Component({
   selector: 'app-dropdown',
   imports: [
-
+    Select2
   ],
   templateUrl: './dropdown.html',
   styleUrl: './dropdown.scss',
 })
 export class Dropdown {
   heading = input.required<string>();
-  values = input.required<Map<string, string>>();
+  values = input.required<DropdownValues>();
 
   selected_value = model<string|null>(null);
 
-  on_change(event: Event): void {
-    event.stopPropagation();
-
-    const target = event.target as HTMLSelectElement;
-
-    this.selected_value.update(() => target.value);
+  on_change(event:  Select2UpdateEvent): void {
+    this.selected_value.update(() => event.value as string);
   }
 }
